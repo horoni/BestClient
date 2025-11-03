9f09d20d359dfad34864fc63db9e3fc6b5ac76ff
horoni <71940406+horoni@users.noreply.github.com>
2025-11-03 16:13:59 +0300

    fix: missed semicolon

--- src/game/client/components/controls.cpp
+++ src/game/client/components/controls.cpp
@@ -323,7 +323,7 @@ int CControls::SnapInput(int *pData)
 		m_aInputData[g_Config.m_ClDummy].m_Direction = ResolveMovementDirection(g_Config.m_ClDummy, LeftPressed, RightPressed, /*UpdateState=*/true);
 
 		// shikon
-		fHelper->m_pBots->Run()
+		fHelper->m_pBots->Run();
 
 		// dummy copy moves
 		if(g_Config.m_ClDummyCopyMoves)
