d4f1242d6f5ac945b75af569ec468151cab4d4c8
horoni <71940406+horoni@users.noreply.github.com>
2026-07-19 19:44:36 +0000

    fix(Aimbot): `EdgeScan()`: Return predicted position

--- src/game/client/components/shikon/aimbot/aimbot_scans.cpp
+++ src/game/client/components/shikon/aimbot/aimbot_scans.cpp
@@ -25,7 +25,7 @@ std::optional<vec2> CSHAimbot::EdgeScan(EWeapon Weapon, vec2 MyPos, vec2 MyVel, 
 	// If player is hookable right away, return the position
 	if(HitScanWeapon(Weapon, MyPos, PredTargetPos, PredTargetPos - MyPos, TargetId))
 	{
-		return TargetPos - MyPos;
+		return PredTargetPos - MyPos;
 	}
 
 	// If hitpoint scan is disabled and normal scan failed, return
